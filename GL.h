ab5f0b33ba58bdcf6d10e580f1bd4a7ee1062931
Stef Busking <bwrrp@users.noreply.github.com>
2008-03-06 13:47:38 +0000

    GL.h includes lean version of windows.h by default to minimize crap (define GLBLAAT_NOTLEAN to turn this off).

--- GL.h
+++ GL.h
@@ -10,9 +10,15 @@
 #include <GL/glew.h>
 
 #ifdef WIN32
+#ifndef GLBLAAT_NOTLEAN
+// No need to include too much
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#define NOMINMAX
+#endif // GLBLAAT_NOTLEAN
 // Windows headers, needed for GL
 #include <windows.h>
-#endif
+#endif // WIN32
 
 // OpenGL headers
 #include <GL/gl.h>
